4d473a821c398f9412d8482c6ac9d367e77c58c8
Júlia Rodrigues <juliardgss@gmail.com>
2019-11-07 16:53:57 -0300

    finalizando api

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
+//iniciando o app
 const app = express()
-
-app.get('/', (req, res) => {
-    res.send('Olá mundo')
+app.use(express.json())
+app.use(cors())
+//iniciando o db
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true
 })
+requireDir('./src/models')
+
+//rotas
+app.use('/api', require('./src/routes'))
 
 app.listen(3001);
